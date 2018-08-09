5e6966c1ad21b09dbbb510a044463fac5f995274
Christopher Aidan Kim <christopheraidank@gmail.com>
2018-08-09 04:31:14 +0000

    First commit

--- db/migrate/0_create_base_tables.rb
+++ db/migrate/0_create_base_tables.rb
@@ -1,5 +1,4 @@
-class CreateBaseTables < ActiveRecord::Migration
-
+class CreateBaseTables < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
       t.string :username
@@ -8,26 +7,21 @@ class CreateBaseTables < ActiveRecord::Migration
       t.string :password
       t.timestamps
     end
-
     create_table :finstagram_posts do |t|
       t.references :user
       t.string :photo_url
       t.timestamps
     end
-
     create_table :comments do |t|
       t.references :user
       t.references :finstagram_post
       t.text :text
       t.timestamps
     end
-
     create_table :likes do |t|
       t.references :user
       t.references :finstagram_post
       t.timestamps
     end
-
   end
-
 end
